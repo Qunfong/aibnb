0c99fad68a40f152bca28623d705fe2e6046dae3
QunWang <qunfong@live.nl>
2024-06-18 19:53:14 +0200

    update

--- src/main/java/com/bnb/ai/AiApplication.java
+++ src/main/java/com/bnb/ai/AiApplication.java
@@ -3,11 +3,20 @@ package com.bnb.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class AiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AiApplication.class, args);
+		int counter = 0;
+		while (true) {
+			counter++;
+			ServiceA serviceA = new ServiceA(new ServiceB(new ServiceC()));
+			serviceA.sendData(UUID.randomUUID().toString(), "Processing data", "Service C");
+		}
 	}
 
 }
